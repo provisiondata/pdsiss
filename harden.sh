c5034a9ff5b12ee0ed89fb31e00074ac02951644
Doug Wilson <d@dkw.io>
2017-07-01 07:10:41 -0700

    Restrict root login, set min password, restrict cron and at, etc.

--- harden.sh
+++ harden.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Centos 7 Hardening 
+# Basic Centos 7 Hardening 
 
 yum makecache fast 
 yum update -y
@@ -21,3 +21,29 @@ echo "enabled = true" >> /etc/fail2ban/jail.local
 
 systemctl enable fail2ban
 systemctl start fail2ban
+
+echo "Root cannot login directly. Use su or sudo."
+echo "tty1" > /etc/securetty
+echo "Disable Root SSH Login"
+sed -ri "s/^#?PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
+echo "Restrict /root directory to root user"
+chmod 700 /root
+
+echo "Minimum passwords length is 8"
+sed -i 's/PASS_MIN_LEN\s+0/PASS_MIN_LEN 8/g' /etc/login.defs
+echo "Change password encryption type to sha512"
+authconfig --passalgo=sha512 --update
+
+echo "Kick inactive users after 20 min."
+echo "readonly TMOUT=1200">> /etc/profile.d/os-security.sh
+echo "readonly HISTFILE" >> /etc/profile.d/os-security.sh
+chmod +x /etc/profile.d/os-security.sh
+
+echo "Restrict cron to root and /etc/cron.allow"
+touch /etc/cron.allow
+chmod 600 /etc/cron.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/cron.deny
+echo "Restrict at to root and /etc/at.allow"
+touch /etc/at.allow
+chmod 600 /etc/at.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/at.deny
