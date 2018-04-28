8da1f4069c7388dad70cd2fdd40dd56284394ca7
Doug Wilson <d@dkw.io>
2018-04-28 11:00:59 -0700

    Trying to fix firewalld and docker conflicts
    https://github.com/moby/moby/issues/16137#issuecomment-271615192

--- init_docker_vm.sh
+++ init_docker_vm.sh
@@ -10,13 +10,23 @@ firewall-cmd --permanent --zone=public --add-service=docker
 firewall-cmd --reload
 
 groupadd docker
-usermod -aG docker $USER
+usermod -aG docker root
 
 yum install -y git device-mapper-persistent-data lvm2  
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum install -y docker-ce
-yum clean all
+
+#https://github.com/moby/moby/issues/16137#issuecomment-271615192
+
+systemctl start NetworkManager.service
+systemctl start docker
+nmcli connection modify docker0 connection.zone trusted
+systemctl stop NetworkManager.service
+firewall-cmd --permanent --zone=trusted --change-interface=docker0
+systemctl start NetworkManager.service
+nmcli connection modify docker0 connection.zone trusted
+systemctl restart docker.service
+systemctl stop NetworkManager.service
 
 systemctl enable docker
-systemctl start docker
